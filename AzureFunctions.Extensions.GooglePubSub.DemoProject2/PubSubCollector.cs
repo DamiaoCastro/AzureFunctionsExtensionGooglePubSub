f40df5006d33cbc8b19ea996e8ea69cb9717e12c
Damiao Castro <damiao.c@hotmail.com>
2018-01-07 17:05:35 +0100

    forgot to remove the disable from the function

--- AzureFunctions.Extensions.GooglePubSub.DemoProject2/PubSubCollector.cs
+++ AzureFunctions.Extensions.GooglePubSub.DemoProject2/PubSubCollector.cs
@@ -3,7 +3,6 @@ using Microsoft.Azure.WebJobs;
 namespace AzureFunctions.Extensions.GooglePubSub.DemoProject2 {
     public static class PubSubCollector {
         [FunctionName("PubSubCollector")]
-        [Disable]
         public static void Run(
            [TimerTrigger("0 */1 * * * *", RunOnStartup = true)]TimerInfo myTimer,
            [GooglePubSub("MyGooglePubSubConfig")] ICollector<string> messages

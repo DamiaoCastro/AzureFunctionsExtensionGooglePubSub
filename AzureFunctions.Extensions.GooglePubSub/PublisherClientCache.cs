aec570462e53734de2da631df5ce210af5b81b51
dcastro <dcastro@travix.com>
2018-01-28 21:42:04 +0100

    create publisherClient cache to fix performance issues in the collector when reading too many times the configuration

---
+++ AzureFunctions.Extensions.GooglePubSub/PublisherClientCache.cs
@@ -0,0 +1,24 @@
+﻿using Google.Cloud.PubSub.V1;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace AzureFunctions.Extensions.GooglePubSub
+{
+    public static class PublisherClientCache
+    {
+        
+        public static ConcurrentDictionary<int, PublisherClient> publisherClientCache = new ConcurrentDictionary<int, PublisherClient>();
+
+        public static PublisherClient GetPublisherClient(GooglePubSubAttribute googlePubSubAttribute)
+        {
+            var key = googlePubSubAttribute.GetHashCode();
+            if (!publisherClientCache.ContainsKey(key))
+            {
+                var publisherClient = CreatorService.GetPublisherClient(googlePubSubAttribute);
+                publisherClientCache.AddOrUpdate(key, publisherClient, (newkey, oldValue) => publisherClient);
+            }
+            return publisherClientCache[key];
+        }
+
+    }
+}

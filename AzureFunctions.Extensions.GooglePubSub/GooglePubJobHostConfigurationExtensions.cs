14dfe0af75efe28035bbba72d946a69c01ea1a02
ksilva_travix <ksilva@travix.com>
2018-08-31 16:33:46 +0200

    Upgrading SDK version beta8

--- AzureFunctions.Extensions.GooglePubSub/GooglePubJobHostConfigurationExtensions.cs
+++ AzureFunctions.Extensions.GooglePubSub/GooglePubJobHostConfigurationExtensions.cs
@@ -9,14 +9,14 @@ namespace AzureFunctions.Extensions.GooglePubSub {
         /// Enables use of the Google PubSub extensions for webjobs
         /// </summary>
         /// <param name="config">The <see cref="JobHostConfiguration"/> to configure.</param>
-        public static void UseGooglePubSub(this JobHostConfiguration config) {
+        public static void UseGooglePubSub(this IWebJobsBuilder config) {
             
             if (config == null) {
                 throw new ArgumentNullException(nameof(config));
             }                     
 
             // Register our extension configuration provider
-            config.RegisterExtensionConfigProvider(new GooglePubSubExtensionConfig());
+            config.AddExtension<GooglePubSubExtensionConfig>();
 
         }
         
--- AzureFunctions.Extensions.GooglePubSub/GooglePubSubExtensionConfig.cs
+++ AzureFunctions.Extensions.GooglePubSub/GooglePubSubExtensionConfig.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Description;
 using Microsoft.Azure.WebJobs.Host.Config;
+using Microsoft.Azure.WebJobs.Logging;
 using Microsoft.Extensions.Logging;
 using System;
 
 namespace AzureFunctions.Extensions.GooglePubSub
 {
-
+    [Extension("googlePubSub")]
     public class GooglePubSubExtensionConfig : IExtensionConfigProvider
     {
+        private readonly ILoggerFactory _loggerFactory;
+        public GooglePubSubExtensionConfig(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
 
         public void Initialize(ExtensionConfigContext context)
         {
@@ -16,12 +23,13 @@ namespace AzureFunctions.Extensions.GooglePubSub
                 throw new ArgumentNullException(nameof(context));
             }
             
-            ILogger logger = context.Config.LoggerFactory.CreateLogger("GooglePubSubExtension");
+            ILogger logger = _loggerFactory.CreateLogger(LogCategories.CreateTriggerCategory("GooglePubSubExtension"));
             
             context.AddBindingRule<GooglePubSubAttribute>()
                 .BindToCollector(c => new AsyncCollector(c, logger));
 
-            context.Config.RegisterBindingExtensions(new TriggerBindingProvider(logger));
+            context.AddBindingRule<GooglePubSubTriggerAttribute>()
+                .BindToTrigger(new TriggerBindingProvider(logger));
 
         }
         
